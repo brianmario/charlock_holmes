6ca29ed4f0a030554e302104d70e27b635bb880c
Brian Lopez <seniorlopez@gmail.com>
2011-08-23 20:29:05 -0700

    some more tests

--- spec/encoding_detector_spec.rb
+++ spec/encoding_detector_spec.rb
@@ -1,33 +1,52 @@
 require 'spec_helper'
 
 describe CharlockHolmes::EncodingDetector do
-  MAPPING = [
-    ['repl2.cljs',                'ISO-8859-1'],
-    ['core.rkt',                  'UTF-8'],
-    ['cl-messagepack.lisp',       'ISO-8859-1'],
-    ['TwigExtensionsDate.es.yml', 'UTF-8'],
-    ['AnsiGraph.psm1',            'UTF-16LE']
-  ]
-
   before :all do
     @detector = CharlockHolmes::EncodingDetector.new
   end
 
-  MAPPING.each do |mapping|
-    file, encoding = mapping
+  test "has a detect class-level method" do
+    CharlockHolmes::EncodingDetector.respond_to? :detect
+    detected = CharlockHolmes::EncodingDetector.detect "test"
+    assert_equal 'ISO-8859-1', detected[:encoding]
+  end
 
-    test "#{file} is detected as #{encoding}" do
-      path = File.expand_path "../fixtures/#{file}", __FILE__
-      content = File.read path
-      guessed = @detector.detect content
+  test "has a detect method" do
+    @detector.respond_to? :detect
+    detected = @detector.detect "test"
+    assert_equal 'ISO-8859-1', detected[:encoding]
+  end
 
-      puts "Guessed: #{guessed.inspect} for file #{file}"
+  test "has a detect_all method" do
+    @detector.respond_to? :detect_all
+    detected_list = @detector.detect_all "test"
+    encoding_list = detected_list.map {|d| d[:encoding]}.sort
+    assert_equal ["ISO-8859-1", "ISO-8859-2", "UTF-8"], encoding_list
+  end
 
-      assert_equal encoding, guessed[:encoding]
+  context "is able to detect that" do
+    MAPPING = [
+      ['repl2.cljs',                'ISO-8859-1'],
+      ['core.rkt',                  'UTF-8'],
+      ['cl-messagepack.lisp',       'ISO-8859-1'],
+      ['TwigExtensionsDate.es.yml', 'UTF-8'],
+      ['AnsiGraph.psm1',            'UTF-16LE']
+    ]
 
-      if content.respond_to? :force_encoding
-        content.force_encoding guessed[:encoding]
-        assert content.valid_encoding?
+    MAPPING.each do |mapping|
+      file, encoding = mapping
+
+      test "#{file} is #{encoding}" do
+        path = File.expand_path "../fixtures/#{file}", __FILE__
+        content = File.read path
+        guessed = @detector.detect content
+
+        assert_equal encoding, guessed[:encoding]
+
+        if content.respond_to? :force_encoding
+          content.force_encoding guessed[:encoding]
+          assert content.valid_encoding?
+        end
       end
     end
   end
