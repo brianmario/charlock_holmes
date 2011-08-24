3c4a925c11cb1a74a664a7ca7611eeb5b6ba1294
Brian Lopez <seniorlopez@gmail.com>
2011-08-24 07:42:47 -0700

    remove homebrew-specific flags

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -3,9 +3,6 @@ require 'mkmf'
 $CFLAGS << ' -Wall -funroll-loops'
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 
-$CFLAGS  << ' -I/usr/local/Cellar/icu4c/4.4.1/include'
-$LDFLAGS << ' -L/usr/local/Cellar/icu4c/4.4.1/lib'
-
 dir_config 'icu'
 
 have_library 'icui18n'
