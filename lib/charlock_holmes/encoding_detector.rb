8bf195aad5ac42de1a8c79eadf2e36c5cf892d58
Brian Lopez <seniorlopez@gmail.com>
2011-08-23 18:41:59 -0700

    docs

--- lib/charlock_holmes/encoding_detector.rb
+++ lib/charlock_holmes/encoding_detector.rb
@@ -1,5 +1,10 @@
 module CharlockHolmes
   class EncodingDetector
+    # Attempt to detect the encoding of this string
+    #
+    # NOTE: This will create a new CharlockHolmes::EncodingDetector instance on every call
+    #
+    # Returns: a Hash with :encoding, :language and :confidence
     def self.detect(str)
       new.detect(str)
     end
