b762e3b087d35b3cfdcc15e57b1d21afa019f0bd
Brian Lopez <seniorlopez@gmail.com>
2011-08-25 11:01:50 -0700

    add String#detect_encodings to string monkey patch (for using detect_all)

--- lib/charlock_holmes/string.rb
+++ lib/charlock_holmes/string.rb
@@ -8,6 +8,15 @@ class String
     encoding_detector.detect(self)
   end
 
+  # Attempt to detect the encoding of this string, and return
+  # a list with all the possible encodings that match it.
+  #
+  # Returns: an Array with zero or more Hashes,
+  #          each one of them with with :encoding, :language and :confidence
+  def detect_encodings
+    encoding_detector.detect_all(self)
+  end
+
   if RUBY_VERSION =~ /1.9/
     # Attempt to detect the encoding of this string
     # then set the encoding to what was detected ala `force_encoding`
--- spec/string_method_spec.rb
+++ spec/string_method_spec.rb
@@ -10,6 +10,17 @@ describe String do
     assert_equal 'ISO-8859-1', detected[:encoding]
   end
 
+  test 'has a detect_encodings method' do
+    str = 'test'
+    str.respond_to? :detect_encodings
+
+    detected_list = str.detect_encodings
+    assert detected_list.is_a? Array
+
+    encoding_list = detected_list.map {|d| d[:encoding]}.sort
+    assert_equal ['ISO-8859-1', 'ISO-8859-2', 'UTF-8'], encoding_list
+  end
+
   if RUBY_VERSION =~ /1.9/
     test 'has a detect_encoding! method' do
       str = 'test'
