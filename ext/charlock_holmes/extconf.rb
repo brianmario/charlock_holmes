2131c1f0f39c2c8c2dd446a139e1f7fcd141a34f
Brian Lopez <seniorlopez@gmail.com>
2011-08-26 17:16:07 -0700

    rearrage extconf a little

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -4,7 +4,9 @@ $CFLAGS << ' -Wall -funroll-loops'
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
 
 dir_config 'icu'
-
 have_library 'icui18n'
+
+dir_config 'magic'
+have_library 'magic'
 
 create_makefile 'charlock_holmes'
