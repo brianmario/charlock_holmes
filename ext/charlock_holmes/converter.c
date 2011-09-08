5f1de2e63ca84df4f4bf6ee0c053cc242bb58d1c
Aman Gupta <aman@tmm1.net>
2011-09-07 23:15:26 -0700

    Fix #3

--- ext/charlock_holmes/converter.c
+++ ext/charlock_holmes/converter.c
@@ -36,7 +36,7 @@ static VALUE rb_converter_convert(VALUE self, VALUE rb_txt, VALUE rb_src_enc, VA
 	}
 
 #ifdef HAVE_RUBY_ENCODING_H
-	(rb_encoding *)rb_enc = rb_enc_find(dst_enc);
+	rb_enc = (void *)rb_enc_find(dst_enc);
 #endif
 
 	rb_out = charlock_new_enc_str(out_buf, out_len, rb_enc);
