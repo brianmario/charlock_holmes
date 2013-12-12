a1b3d7cac1db9cdf6349b4cb2f7d3bc602095c8a
Geoff <geoff.nixon@aol.com>
2013-12-11 22:48:56 -0800

    Update libmagic; link system ICU on Darwin; use RbConfig CFLAGS, etc.
    This pull request includes changes intended to allow the resolution of #29, #40, #42 (partial), #43, #49, #50, #51, #52.
    
    - file/libmagic (and patch) updated to 5.16.
    - extconf.rb modified to:
      - enforce RbConfig compiler and related flags.
      - fix for non-GNU 'make' (ie, no ```make -C```).
      - remove 'dst' build directory after compiling.
      - **Build and link against system ICU on Darwin.**
        I've included a minimal subset of the ICU headers in ```darwin-icu-headers.tar.gz```,
        taken from the [Apple ICU](http://www.opensource.apple.com/source/ICU/ICU-461.18) source (Apple doesn't distribute these headers because they prefer you use their client frameworks like Core Text, etc., but they're available freely at the link above). These headers are from the version bundled with 10.7 (10.6 was too old, so I've retained the check for homebrew icu4c on older versions).
    
        I've tested that they work on 10.9, so presumably it should work on 10.7 and 10.8 as well.
        If anyone can confirm that, that would be great.
    
        I think perhaps this is a better solution than relying on ```brew install icu4c```?

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -17,43 +17,62 @@ if `which make`.strip.empty?
   exit(1)
 end
 
+# Make sure we're using the right compiler and flags.
+
+ENV['CC']       = RbConfig::MAKEFILE_CONFIG['CC']
+ENV['CXX']      = RbConfig::MAKEFILE_CONFIG['CXX']
+ENV['CFLAGS']   = '-O2 -fPIC -std=gnu99'
+ENV['CXXFLAGS'] = '-O2 -fPIC'
+
+# Make sure we're using our headers rather than ones elsewhere.
+$INCFLAGS[0,0] = " -I#{CWD} "; $INCFLAGS << " -I#{CWD} "
+$LDFLAGS << " -L#{CWD} "
+
 ##
 # ICU dependency
 #
 
 dir_config 'icu'
 
-# detect homebrew installs
-if !have_library 'icui18n'
-  base = if !`which brew`.empty?
-    `brew --prefix`.strip
-  elsif File.exists?("/usr/local/Cellar/icu4c")
-    '/usr/local/Cellar'
-  end
+if %x(uname).chomp == "Darwin" # Use the system ICU on Darwin 10.7+
+  if %x(sw_vers -productVersion | cut -c4-5).chomp.to_i > 6
+    
+    %x(cd #{CWD}; tar xf src/darwin-icu-headers.tar.gz)
+    $LDFLAGS << " -L/usr/lib "; $LDFLAGS.insert(0," -L/usr/lib -Wl,-search_dylibs_first ")
+    have_library 'icucore' and have_header 'unicode/ucnv.h' or abort 'ICU missing.' 
 
-  if base and icu4c = Dir[File.join(base, 'Cellar/icu4c/*')].sort.last
-    $INCFLAGS << " -I#{icu4c}/include "
-    $LDFLAGS  << " -L#{icu4c}/lib "
-  end
+  elsif not have_library 'icui18n' # Otherwise check Homebrew.
+
+    base = if not %x(which brew).empty? %x(brew --prefix).strip
+           elsif File.exists?("/usr/local/Cellar/icu4c") then '/usr/local/Cellar' end
+    if base and icu4c = Dir[File.join(base, 'Cellar/icu4c/*')].sort.last
+      $INCFLAGS << " -I#{icu4c}/include "
+      $LDFLAGS  << " -L#{icu4c}/lib "
+
+      unless have_library 'icui18n' and have_header 'unicode/ucnv.h'
+        STDERR.puts "\n\n"
+        STDERR.puts "**********************************************************"
+        STDERR.puts "*********** icu required (brew install icu4c) ************"
+        STDERR.puts "**********************************************************"
+        exit(1)
+      end
+    end
+  end # !Darwin
+  else
+    unless have_library 'icui18n' and have_library 'icudata' and have_header 'unicode/ucnv.h' 
+      STDERR.puts "\n\n"
+      STDERR.puts "***********************************************************************"
+      STDERR.puts "*********** icu required (i.e., apt-get install libicu-dev) ***********"
+      STDERR.puts "***********************************************************************"
+      exit(1)
+    end
 end
-
-unless have_library 'icui18n' and have_header 'unicode/ucnv.h'
-  STDERR.puts "\n\n"
-  STDERR.puts "***************************************************************************************"
-  STDERR.puts "*********** icu required (brew install icu4c or apt-get install libicu-dev) ***********"
-  STDERR.puts "***************************************************************************************"
-  exit(1)
-end
-
-have_library 'z' or abort 'libz missing'
-have_library 'icuuc' or abort 'libicuuc missing'
-have_library 'icudata' or abort 'libicudata missing'
 
 ##
 # libmagic dependency
 #
 
-src = File.basename('file-5.08.tar.gz')
+src = File.basename('file-5.16.tar.gz')
 dir = File.basename(src, '.tar.gz')
 libdir = 'lib'
 
@@ -62,19 +81,19 @@ Dir.chdir("#{CWD}/src") do
 
   sys("tar zxvf #{src}")
   Dir.chdir(dir) do
-    sys("./configure --prefix=#{CWD}/dst/ --disable-shared --enable-static --with-pic")
+    sys("./configure --prefix=#{CWD}/dst --disable-shared --enable-static --with-pic")
     sys("patch -p0 < ../file-soft-check.patch")
-    sys("make -C src install")
-    sys("make -C magic install")
+    sys("cd src; make install")
+    sys("cd magic; make install")
     # this should evaluate to either 'lib' or 'lib64'
     libdir = `grep ^libdir config.log`.strip().split("'")[1].split('/')[-1]
   end
 end
 
 FileUtils.cp "#{CWD}/dst/#{libdir}/libmagic.a", "#{CWD}/libmagic_ext.a"
+FileUtils.cp "#{CWD}/dst/include/magic.h",      "#{CWD}/magic.h"
 
-$INCFLAGS[0,0] = " -I#{CWD}/dst/include "
-$LDFLAGS << " -L#{CWD} "
+%x(cd #{CWD}; rm -rf dst)
 
 dir_config 'magic'
 unless have_library 'magic_ext' and have_header 'magic.h'
