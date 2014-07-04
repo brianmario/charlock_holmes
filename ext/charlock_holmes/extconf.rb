707aaa0ed21b865ffcdc1d951aed2c6454f59e42
Lourens Naudé <lourens@methodmissing.com>
2014-07-04 19:36:46 +0100

    Oust libmagic dependency

--- ext/charlock_holmes/extconf.rb
+++ ext/charlock_holmes/extconf.rb
@@ -48,42 +48,8 @@ unless have_library 'icui18n' and have_library 'icudata' and have_library 'icutu
   exit(1)
 end
 
-##
-# libmagic dependency
-#
-
-src = File.basename('file-5.08.tar.gz')
-dir = File.basename(src, '.tar.gz')
-
-Dir.chdir("#{CWD}/src") do
-  FileUtils.rm_rf(dir) if File.exists?(dir)
-
-  sys("tar zxvf #{src}")
-  Dir.chdir(dir) do
-    sys("./configure --prefix=#{CWD}/dst/ --disable-shared --enable-static --with-pic")
-    sys("patch -p0 < ../file-soft-check.patch")
-    sys("make -C src install")
-    sys("make -C magic install")
-  end
-end
-
-FileUtils.cp "#{CWD}/dst/lib/libmagic.a", "#{CWD}/libmagic_ext.a"
-
-$INCFLAGS[0,0] = " -I#{CWD}/dst/include "
-$LDFLAGS << " -L#{CWD} "
-
-dir_config 'magic'
-unless have_library 'magic_ext' and have_header 'magic.h'
-  STDERR.puts "\n\n"
-  STDERR.puts "***************************************************************************************"
-  STDERR.puts "********* error compiling and linking libmagic. please report issue on github *********"
-  STDERR.puts "***************************************************************************************"
-  exit(1)
-end
-
 $CFLAGS << ' -Wall -funroll-loops'
 $CFLAGS << ' -Wextra -O0 -ggdb3' if ENV['DEBUG']
-$LIBS << " -lstdc++"
 
 ENV['RUBYOPT'] = rubyopt
 create_makefile 'charlock_holmes/charlock_holmes'
