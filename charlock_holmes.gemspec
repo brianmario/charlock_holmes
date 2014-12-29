brianmario/charlock_holmes

commit 27077ae6c5c455b568b215540241d6ec20305216

cleanup gemspec

charlock_holmes.gemspec
@@ -1,23 +1,15 @@
-# encoding: utf-8
-
 require './lib/charlock_holmes/version' unless defined? CharlockHolmes::VERSION
 
-Gem::Specification.new do |s|
-  s.name = %q{charlock_holmes}
+Gem::Specification.new "charlock_holmes", CharlockHolmes::VERSION do |s|
   s.license = "MIT"
-  s.version = CharlockHolmes::VERSION
   s.authors = ["Brian Lopez", "Vicent Martí"]
-  s.date = Time.now.utc.strftime("%Y-%m-%d")
-  s.email = %q{seniorlopez@gmail.com}
+  s.email = "seniorlopez@gmail.com"
   s.extensions = ["ext/charlock_holmes/extconf.rb"]
-  s.files = `git ls-files`.split("\n")
-  s.homepage = %q{https://github.com/brianmario/charlock_holmes}
+  s.files = `git ls-files lib`.split("\n")
+  s.homepage = "https://github.com/brianmario/charlock_holmes"
   s.rdoc_options = ["--charset=UTF-8"]
-  s.require_paths = ["lib"]
-  s.rubygems_version = %q{1.4.2}
-  s.summary = %q{Character encoding detection, brought to you by ICU}
+  s.summary = "Character encoding detection, brought to you by ICU"
   s.description = "charlock_holmes provides binary and text detection as well as text transcoding using libicu"
-  s.test_files = `git ls-files spec`.split("\n")
   s.required_ruby_version = '>= 1.9.3'
 
   # tests

commit cbf24f15f41711f74a1364d59ec696be71ecec51

Include ext/ files in gem
Regression from 27077ae6c5c455b568b215540241d6ec20305216

charlock_holmes.gemspec
@@ -5,7 +5,7 @@ Gem::Specification.new "charlock_holmes", CharlockHolmes::VERSION do |s|
   s.authors = ["Brian Lopez", "Vicent Martí"]
   s.email = "seniorlopez@gmail.com"
   s.extensions = ["ext/charlock_holmes/extconf.rb"]
-  s.files = `git ls-files lib`.split("\n")
+  s.files = `git ls-files ext lib`.split("\n")
   s.homepage = "https://github.com/brianmario/charlock_holmes"
   s.rdoc_options = ["--charset=UTF-8"]
   s.summary = "Character encoding detection, brought to you by ICU"